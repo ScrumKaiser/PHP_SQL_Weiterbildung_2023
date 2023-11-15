62c454488994b76e0edbc8789c8280d4c0dac6e7
Oliver Noll <langustee@gmail.com>
2023-11-15 16:05:15 +0100

    First try

---
+++ firstSite/form.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+    <body>
+        <input type="text" name="username" value="<?= 'Mr. X' ?>" />
+        <input type="password" name="password" value="<?= '*****' ?>" />
+        <input type="submit" value="login" />
+    </body>
+</html>
